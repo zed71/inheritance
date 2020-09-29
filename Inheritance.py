1af572b7803025cf2fa7ce6cb963e2aabd870cf5
BrandonF <37319101+zed71@users.noreply.github.com>
2020-09-28 23:31:14 -0700

    Add file to commit
    Inheritance file

---
+++ Inheritance.py
@@ -0,0 +1,14 @@
+class Book:
+    genre = 'No Genre Provided'
+    author = 'No Author Provided'
+    Publisher = 'No Publisher Provided'
+    Year = 0
+
+class Story(Book):
+    storyline = 'Fantasy'
+    department = 'Medical Fantasy'
+
+class Work(Book):
+    classtype = 'computer coding'
+    author ='tech academy'
+    year = 2020
